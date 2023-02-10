cb8e87910ddf6b56de9a58cab8cb02e3537c5325
AbhishekSatpathy4848 <abhisheksatpathy.211it002@nitk.edu.in>
2023-02-10 21:02:32 +0530

    added code

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,8 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'package:ngo_hackathon/features/login/registration/login.dart';
+import 'package:ngo_hackathon/features/login&registration/login.dart';
 import 'package:ngo_hackathon/home.dart';
-// import 'firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
