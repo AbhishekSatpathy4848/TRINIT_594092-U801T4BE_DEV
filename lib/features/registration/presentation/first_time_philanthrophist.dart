751653cd2d8e8bddbfde257348e9bc9cee31e7cc
Shashank-Girish <96805090+Shashank-Girish@users.noreply.github.com>
2023-02-11 14:09:01 +0530

    Modified first time phil reg

--- lib/features/registration/presentation/first_time_philanthrophist.dart
+++ lib/features/registration/presentation/first_time_philanthrophist.dart
@@ -1,9 +1,12 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
 import 'package:ngo_hackathon/core/models/philanthropist_model.dart';
+import 'package:ngo_hackathon/core/pages/home.dart';
+import 'package:ngo_hackathon/root_page.dart';
 
 class NewPhilanthrophist extends StatefulWidget {
   const NewPhilanthrophist({super.key});
@@ -13,6 +16,16 @@ class NewPhilanthrophist extends StatefulWidget {
 }
 
 class _NewPhilanthrophistState extends State<NewPhilanthrophist> {
+  void dispose() {
+    nameController.dispose();
+    phone.dispose();
+    city.dispose();
+    state.dispose();
+    desc.dispose();
+    super.dispose();
+  }
+
+  final email = FirebaseAuth.instance.currentUser!.email;
   TextEditingController nameController = TextEditingController();
   TextEditingController phone = TextEditingController();
   TextEditingController city = TextEditingController();
@@ -21,162 +34,160 @@ class _NewPhilanthrophistState extends State<NewPhilanthrophist> {
   bool firstTimeLogin = true;
   @override
   Widget build(BuildContext context) {
-    return Container(
-        child: Column(children: [
-      Column(
-        children: [
-          const Text(
-            'Name',
-            style: TextStyle(fontSize: 14),
-          ),
-          TextFormField(
-            controller: nameController,
-            decoration: const InputDecoration(
-              hintText: 'Enter your name',
-              hintStyle: TextStyle(fontSize: 14),
+    return SingleChildScrollView(
+      child: Container(
+          child: Column(children: [
+        Column(
+          children: [
+            const Text(
+              'Name',
+              style: TextStyle(fontSize: 14),
             ),
-            validator: (value) {
-              if (value == null || value.isEmpty) {
-                return 'Please enter your name';
-              }
-              return null;
-            },
-          ),
-        ],
-      ),
-      const SizedBox(
-        height: 5,
-      ),
-      Column(
-        children: [
-          const Text(
-            'Phone Number',
-            style: TextStyle(fontSize: 14),
-          ),
-          TextFormField(
-            controller: phone,
-            decoration: const InputDecoration(
-              hintText: 'Enter your Phone Number',
-              hintStyle: TextStyle(fontSize: 14),
+            TextFormField(
+              controller: nameController,
+              decoration: const InputDecoration(
+                hintText: 'Enter your name',
+                hintStyle: TextStyle(fontSize: 14),
+              ),
+              validator: (value) {
+                if (value == null || value.isEmpty) {
+                  return 'Please enter your name';
+                }
+                return null;
+              },
             ),
-            validator: (value) {
-              if (value == null || value.isEmpty) {
-                return 'Please enter your Phone Number';
-              }
-              return null;
-            },
-          ),
-        ],
-      ),
-      const SizedBox(
-        height: 5,
-      ),
-      Column(
-        children: [
-          const Text(
-            'City',
-            style: TextStyle(fontSize: 14),
-          ),
-          TextFormField(
-            controller: city,
-            decoration: const InputDecoration(
-              hintText: 'Enter your City',
-              hintStyle: TextStyle(fontSize: 14),
+          ],
+        ),
+        const SizedBox(
+          height: 5,
+        ),
+        Column(
+          children: [
+            const Text(
+              'Phone Number',
+              style: TextStyle(fontSize: 14),
             ),
-            validator: (value) {
-              if (value == null || value.isEmpty) {
-                return 'Please enter your city';
-              }
-              return null;
-            },
-          ),
-        ],
-      ),
-      const SizedBox(
-        height: 5,
-      ),
-      Column(
-        children: [
-          const Text(
-            'State',
-            style: TextStyle(fontSize: 14),
-          ),
-          TextFormField(
-            controller: state,
-            decoration: const InputDecoration(
-              hintText: 'Enter your State',
-              hintStyle: TextStyle(fontSize: 14),
+            TextFormField(
+              controller: phone,
+              keyboardType: TextInputType.phone,
+              decoration: const InputDecoration(
+                hintText: 'Enter your Phone Number',
+                hintStyle: TextStyle(fontSize: 14),
+              ),
+              validator: (value) {
+                if (value == null || value.isEmpty) {
+                  return 'Please enter your Phone Number';
+                }
+                return null;
+              },
             ),
-            validator: (value) {
-              if (value == null || value.isEmpty) {
-                return 'Please enter your State';
-              }
-              return null;
-            },
-          ),
-        ],
-      ),
-      const SizedBox(
-        height: 5,
-      ),
-      Column(
-        children: [
-          const Text(
-            'Brief Description',
-            style: TextStyle(fontSize: 14),
-          ),
-          TextFormField(
-            controller: desc,
-            decoration: const InputDecoration(
-              hintText: 'Enter a brief description about yourself',
-              hintStyle: TextStyle(fontSize: 14),
+          ],
+        ),
+        const SizedBox(
+          height: 5,
+        ),
+        Column(
+          children: [
+            const Text(
+              'City',
+              style: TextStyle(fontSize: 14),
             ),
-            validator: (value) {
-              if (value == null || value.isEmpty) {
-                return 'This field cannot be left blank';
+            TextFormField(
+              controller: city,
+              decoration: const InputDecoration(
+                hintText: 'Enter your City',
+                hintStyle: TextStyle(fontSize: 14),
+              ),
+              validator: (value) {
+                if (value == null || value.isEmpty) {
+                  return 'Please enter your city';
+                }
+                return null;
+              },
+            ),
+          ],
+        ),
+        const SizedBox(
+          height: 5,
+        ),
+        Column(
+          children: [
+            const Text(
+              'State',
+              style: TextStyle(fontSize: 14),
+            ),
+            TextFormField(
+              controller: state,
+              decoration: const InputDecoration(
+                hintText: 'Enter your State',
+                hintStyle: TextStyle(fontSize: 14),
+              ),
+              validator: (value) {
+                if (value == null || value.isEmpty) {
+                  return 'Please enter your State';
+                }
+                return null;
+              },
+            ),
+          ],
+        ),
+        const SizedBox(
+          height: 5,
+        ),
+        Column(
+          children: [
+            const Text(
+              'Brief Description',
+              style: TextStyle(fontSize: 14),
+            ),
+            TextFormField(
+              controller: desc,
+              decoration: const InputDecoration(
+                hintText: 'Enter a brief description about yourself',
+                hintStyle: TextStyle(fontSize: 14),
+              ),
+              validator: (value) {
+                if (value == null || value.isEmpty) {
+                  return 'This field cannot be left blank';
+                }
+                return null;
+              },
+            ),
+          ],
+        ),
+        const SizedBox(
+          height: 5,
+        ),
+        ElevatedButton(
+            onPressed: () {
+              if (nameController.text.isNotEmpty &&
+                  phone.text.isNotEmpty &&
+                  city.text.isNotEmpty &&
+                  state.text.isNotEmpty &&
+                  desc.text.isNotEmpty) {
+                PhilanthropistModel philanthropistModel = PhilanthropistModel(
+                    name: nameController.text,
+                    email: email,
+                    phoneNo: phone.text,
+                    profilePhoto:
+                        'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png',
+                    preferredAreaofimpact: ['Education', 'Healthcare'],
+                    followingNgo: null,
+                    city: city.text,
+                    state: state.text,
+                    communities: null,
+                    description: desc.text,
+                    firstTimeLogin: firstTimeLogin);
+                final docUser =
+                    FirebaseFirestore.instance.collection('users').doc();
+                docUser.set({'email': email, 'type': 'Philanthropist'});
+                philanthropistModel.createPhilanthropist(philanthropistModel);
+                Navigator.pushReplacement(context,
+                    MaterialPageRoute(builder: (context) => const RootPage()));
               }
-              return null;
             },
-          ),
-        ],
-      ),
-      const SizedBox(
-        height: 5,
-      ),
-      ElevatedButton(
-          onPressed: () {
-            if (nameController.text.isNotEmpty &&
-                phone.text.isNotEmpty &&
-                city.text.isNotEmpty &&
-                state.text.isNotEmpty &&
-                desc.text.isNotEmpty) {
-              PhilanthropistModel philanthropistModel = PhilanthropistModel(
-                  name: nameController.text,
-                  email: 'shashankgirish07@gmail.com',
-                  phoneNo: phone.text,
-                  profilePhoto:
-                      'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png',
-                  preferredAreaofimpact: ['Education', 'Healthcare'],
-                  followingNgo: ['Ngo1', 'Ngo2'],
-                  city: city.text,
-                  state: state.text,
-                  communities: null,
-                  description: desc.text,
-                  firstTimeLogin: firstTimeLogin);
-              final docUser =
-                  FirebaseFirestore.instance.collection('users').doc();
-              docUser.set({
-                'email': 'shashankgirish07@gmail.com',
-                'type': 'philanthropist'
-              });
-              philanthropistModel.createPhilanthropist(philanthropistModel);
-              // Navigator.push(
-              //     context,
-              //     MaterialPageRoute(
-              //         builder: (context) => const NewPhilanthrophist()));
-            }
-          },
-          child: const Text('Submit'))
-    ]));
+            child: const Text('Submit'))
+      ])),
+    );
   }
 }
--- lib/root_page.dart
+++ lib/root_page.dart
@@ -1,3 +1,5 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:ngo_hackathon/core/pages/search_page.dart';
 import 'package:ngo_hackathon/features/registration/presentation/first_time_philanthrophist.dart';
@@ -13,36 +15,75 @@ class RootPage extends StatefulWidget {
 }
 
 class _RootPageState extends State<RootPage> {
+  bool firstTimeLogin = true;
+  bool showNavBar = true;
+
+  Future checkData() async {
+    await FirebaseFirestore.instance.collection('users').get().then((value) => {
+          value.docs.forEach((element) {
+            if (element['email'] == FirebaseAuth.instance.currentUser!.email) {
+              firstTimeLogin = false;
+              showNavBar = true;
+              //print(firstTimeLogin);
+            }
+          })
+        });
+    //setState(() {});
+  }
+
   int currentpage = 0;
   final screens = [
-    const NewPhilanthrophist(),
+    const HomePage(),
     const SearchPage(),
     const ProfilePage(),
   ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          FirebaseAuth.instance.signOut();
+        },
+        child: const Icon(Icons.logout),
+      ),
       appBar: AppBar(
         elevation: 0.0,
         automaticallyImplyLeading: false,
-        title: Center(
-          child: Text('NGO-app'),
+        title: const Center(
+          child: Text('NGO-App'),
         ),
       ),
-      body: IndexedStack(
-        index: currentpage,
-        children: screens,
-      ),
+      body: FutureBuilder(
+          future: checkData(),
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return const Center(
+                child: CircularProgressIndicator(),
+              );
+            } else if (snapshot.connectionState == ConnectionState.done &&
+                firstTimeLogin) {
+              //print('abc');
+              return const NewPhilanthrophist();
+            }
+            //print('123');
+
+            return IndexedStack(
+              index: currentpage,
+              children: screens,
+            );
+          }),
       bottomNavigationBar: NavigationBar(
         destinations: const [
-          NavigationDestination(icon: Icon(Icons.home), label: 'home'),
-          NavigationDestination(icon: Icon(Icons.search), label: 'explore'),
-          NavigationDestination(icon: Icon(Icons.person), label: 'profile')
+          NavigationDestination(icon: Icon(Icons.home), label: 'Home'),
+          NavigationDestination(icon: Icon(Icons.search), label: 'Explore'),
+          NavigationDestination(icon: Icon(Icons.person), label: 'Profile')
         ],
         onDestinationSelected: (int index) {
-          setState(() {
-            currentpage = index;
-          });
+          if (firstTimeLogin == false) {
+            setState(() {
+              currentpage = index;
+            });
+          }
         },
         selectedIndex: currentpage,
       ),
